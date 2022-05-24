546bb61d67c26c65b29a4b125b0f4fa4eb3f1f4b
Johannes <johannes@hald.com>
2022-05-24 22:55:21 +0200

    Add files via upload

---
+++ weather app/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace WetterApp;
+
+class Program {
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Die aktuelle Temperatur in welcher Stadt möchtest du wissen?");
+        string city = Console.ReadLine();
+
+        HttpClient httpClient = new HttpClient();
+        string requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=98fddd6a43823c753014981b0c01911b&units=metric";
+        HttpResponseMessage httpResponse = httpClient.GetAsync(requestUrl).Result;
+        string response = httpResponse.Content.ReadAsStringAsync().Result;
+        WeatherMapResponse weatherMapResponse = JsonConvert.DeserializeObject<WeatherMapResponse>(response);
+        Console.WriteLine("In " + city + " hat es aktuell " + weatherMapResponse.main.temp + "°C");
+        Console.ReadKey();
+    }
+}
+
+
+class WeatherMapResponse
+    {
+        public Main main;
+    }
+
+
+
+
+class Main
+{
+    public float temp;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
